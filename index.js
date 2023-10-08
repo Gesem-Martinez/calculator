396ead22af183d3d8b68bc03361a9ef1d3a65897
Gesem Martinez <ghmartinezmontes@gmail.com>
2023-10-08 11:31:00 -0600

    Change populateDisplay function. Add event listener to equals button. Change operate function to accept more than two numbers

--- index.js
+++ index.js
@@ -14,34 +14,75 @@ function divide(num1, num2){
     return num1 / num2;
 }
 
-function operate(operator, num1, num2){
-    switch(operator){
-        case "+":
-            add(num1, num2);
-            break;
-        case "-":
-            substract(num1, num2);
-            break;
-        case "x":
-            multiply(num1, num2);
-            break;
-        case "/":
-            divide(num1, num2);
-            break;
+function operate(operators, numbers){
+    let result = numbers[0];
+
+    for(let i = 0; i < operators.length; i++){
+        let operator = operators[i];
+
+        switch(operator){
+            case "+":
+                result = add(result, numbers[i + 1]);
+                break;
+            case "-":
+                result = substract(result, numbers[i + 1]);
+                break;
+            case "x":
+                result = multiply(result, numbers[i + 1]);
+                break;
+            case "/":
+                result = divide(result, numbers[i + 1]);
+                break;
+        }
     }
+
+    return result;
+}
+
+function clearDisplay(){
+    document.querySelector(".display").textContent = "";
 }
 
 function populateDisplay(string){
-    
+    let display = document.querySelector(".display");
+    let displayContent = display.textContent;
+
+    if(displayContent.length === 10){
+        return;
+    }
+
+    display.textContent += string;
 }
 
-let number1 = 0;
-let operator = "";
-let number2 = 0;
+let operators = [];
+let numbers = [];
 
 let numberButtons = Array.from(document.querySelectorAll("button")).filter(
-    (button) => !Number.isNaN(Number.parseInt(button.textContent)));
+    (button) => !Number.isNaN(Number.parseInt(button.textContent))
+);
+let specialButtons = Array.from(document.querySelectorAll("button")).filter(
+    (button) => Number.isNaN(Number.parseInt(button.textContent)) 
+    && button.textContent != "CLEAR" 
+    && button.textContent != "DEL"
+    && button.textContent != "="
+);
 
-numberButtons.forEach(button => {
+let printableButtons = numberButtons.concat(specialButtons);
+
+let equalsBtn = document.querySelector(".equals");
+let clearBtn = document.querySelector(".clear");
+
+printableButtons.forEach(button => {
     button.addEventListener("click", event => populateDisplay(event.target.textContent));
 });
+
+equalsBtn.addEventListener("click", (event) =>{
+    numbers = Number.parseFloat(document.querySelector(".display").textContent.split(/[\/\x\-\+]/g));
+    operators = document.querySelector(".display").textContent.match(/[\/\x\-\+]/g);
+
+    let result = operate(operators, numbers);
+    clearDisplay();
+    populateDisplay(result);
+});
+
+clearBtn.addEventListener("click", () => clearDisplay());
