a4690af7e6599cdc6afbc2b9e73d2a08ea9087c5
Gesem Martinez <ghmartinezmontes@gmail.com>
2023-10-08 00:02:39 -0600

    Add basic calculator functions

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("Hola");
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function substract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            substract(num1, num2);
+            break;
+        case "x":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+    }
+}
+
+let number1 = 0;
+let operator = "";
+let number2 = 0;
